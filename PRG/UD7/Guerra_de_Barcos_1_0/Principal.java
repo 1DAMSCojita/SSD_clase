32980003ff1c38f3ce6a3dbe746579950fa37a2c
1DAMSCojita <scojita20207@iesjoanramis.org>
2024-02-05 19:52:39 +0100

    Actualizando barcos.

--- PRG/UD7/Guerra_de_Barcos_1_0/Principal.java
+++ PRG/UD7/Guerra_de_Barcos_1_0/Principal.java
@@ -3,5 +3,9 @@ package Guerra_de_Barcos_1_0;
 public class Principal {
     public static void main(String[] args) {
         
+        Tablero t = new Tablero();
+
+        t.mostrar_tablero(); 
+
     }
 }
